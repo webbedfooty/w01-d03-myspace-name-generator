5e670003395ce090df0cf8f9a6465738b913d3f1
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-16 16:43:10 -0500

    Refresh

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -1,4 +1,10 @@
-# You'll have to create your own method(s) here and define them. Use patterns like
-# you've seen elsewhere.
+# You'll have to create your own method(s) here and define them.
+# Use patterns like you've seen elsewhere.
+#
+#   Prepend with xX_
+#   Append with Xx_
+#   Remove all whitespace (spaces, tabs, newline) and punctuation
+#   Alternate caps and lowercase
+#   for example: Andrew Ek should become xX_AnDrEwEk_Xx
 
 require 'pry'
