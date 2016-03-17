8cd9d5ea96300f41ec37e7dcebda59dba6e209fc
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-16 22:14:03 -0500

    stuck

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -7,4 +7,29 @@
 #   Alternate caps and lowercase
 #   for example: Andrew Ek should become xX_AnDrEwEk_Xx
 
-require 'pry'
+myspace_name = "Andew  EEK  "
+
+#require 'pry'
+#def name_generator(myspace_name)
+nc = myspace_name.length
+x = 0
+y = 0
+first_scrub = myspace_name.delete(' ')
+while x != nc-1
+first_scrub = first_scrub.sub(/[:.,?!\'\"]/, '')
+x = x + 1
+end
+#puts first_scrub
+first_scrub = first_scrub.upcase
+
+#puts first_scrub
+generator = Array.new
+
+while y != nc-1
+generator[y] = first_scrub.byteslice(y)
+#generator[y] = generator[y] #.downcase
+y = y + 1
+generator[y] = first_scrub.byteslice(y)
+y = y + 1
+end
+puts generator
