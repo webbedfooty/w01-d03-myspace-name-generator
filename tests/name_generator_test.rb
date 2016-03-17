f3cf79b239bf7b957a2e22b78c76a6e30b56a412
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-16 19:28:52 -0500

    figured out punctuation

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -9,7 +9,7 @@ require_relative '../lib/name_generator.rb'
 #   for example: Andrew Ek should become xX_AnDrEwEk_Xx
 
 # Now run your actual tests, making sure to do output as necessary
-myspace_name = " xX_A n:Dr EwEk_ Xx "
+myspace_name = " xX_A: n:Dr .,EwEk_ Xx "
 
 # Test 1 - does the first 3 characters of myspace_name match 'xX_'
 test_01 = myspace_name[0..2]
@@ -32,9 +32,12 @@ end
 
 # Test 3 - has all white space/punctuation been removed (special characters)
 
-
+x = 0
 first_scrub = myspace_name.delete(' ')
+while x != nc-1
+first_scrub = first_scrub.sub(/[:.,]/, '')
+x = x + 1
+end
 puts first_scrub
-second_scrub = first_scrub.sub(/[:]/, '')
-puts second_scrub
+
 # Test 4 - do the letters alternate from lowercase to uppercase
